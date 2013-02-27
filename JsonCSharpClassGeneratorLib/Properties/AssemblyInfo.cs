4128765f3c766829003eaa3888514291b7e2cc2c
Andrea Martinelli <martinelli-andrea@outlook.com>
2013-02-27 16:45:24 +0100

    updated version info

--- JsonCSharpClassGeneratorLib/Properties/AssemblyInfo.cs
+++ JsonCSharpClassGeneratorLib/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Json C# Class Generator")]
+[assembly: AssemblyTitle("Json Class Generator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Xamasoft")]
-[assembly: AssemblyProduct("Json C# Class Generator")]
+[assembly: AssemblyProduct("Json Class Generator")]
 [assembly: AssemblyCopyright("Copyright © Xamasoft 2010-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
